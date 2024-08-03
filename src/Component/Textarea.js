525e68d0a11eb182927e493d4ae443c4538a5ed5
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-08-03 12:18:03 +0530

    day 5

--- src/Component/Textarea.js
+++ src/Component/Textarea.js
@@ -2,37 +2,112 @@ import React, {useState} from 'react'
 
 
 export default function Textarea(props) {
-    const handleUpClick=()=> {
-        console.log ("Upper Case was click + Text");
-        setText("You have ckicked on handleUpClick")
+    
+        const handleSentenceClick = () => {
+            console.log("Sentence Case was clicked + text");
+            let newtext = text.toLowerCase().split('. ').map(sentence => {
+                return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+            }).join('. ');
+            setText(newtext);
+        };
+        const handleUpClick=()=> {
+        console.log ("Upper Case was click + text");
+            let newtext = text.toUpperCase();
+        setText(newtext);
         
+    };
+    const handleloClick=()=> {
+        console.log ("Upper Case was click + text");
+            let newtext = text.toLowerCase();
+        setText(newtext);
+        
+    };
+    const handleAlternatingClick = () => {
+        console.log("Alternating Case was clicked + text");
+        let newtext = text.split('').map((char, index) => {
+            return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+        }).join('');
+        setText(newtext);
+    };
+
+    const handleInversrClick = () => {
+        console.log("Inverse Case was clicked + text");
+        let newtext = text.split('').map(char => {
+            if (char === char.toUpperCase()) {
+                return char.toLowerCase();
+            } else {
+                return char.toUpperCase();
+            }
+        }).join('');
+        setText(newtext);
+    };
+
+    const handleCapitalizeClick = () => {
+        console.log("Capitalize Case was clicked + text");
+        let newtext = text.toLowerCase().split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }).join(' ');
+        setText(newtext);
+    };
+
+    const handleclearClick=()=> {
+        console.log ("Clear was clicked + text");
+        setText("");        
+    }
+    const handlecopyClick=()=> {
+        console.log ("Copy was clicked + text");
+        navigator.clipboard.writeText(text);        
     }
 
-    const handleOnChange=()=> {
-        console.log ("onChange");
+    const handleOnChange=(event)=> {
+        //console.log ("onChange");
         setText(event.target.value)
        
     }
 
-   const [Text, setText]= useState('Enter text here'); 
+   const [text, setText]= useState('Enter text'); 
 // text = "new text";  wrong way to set the text.
-//setText("new text")
+//settext("new text")
     
     return (
-    
-        <div>
-
-                    <h1> {props.heading}  </h1>
+    <>
+        
+                    <h1 mx-5> {props.heading} </h1>
 
                     
 
-                    <textarea id="Text" className="Text" rows="10" cols="150" value={Text} onChange={handleOnChange}>
+                    <textarea id="text" className="text" rows="10" cols="150" value={text} onChange={handleOnChange}>
                    
                     </textarea>
 
                     <div>
-                         <button className='btn btn-primary' onClick={handleUpClick}> Convert to upper case </button>
+                            <button className='btn btn-primary mx-2 my-2' onClick={handleSentenceClick} > Sentance Case </button>
+
+                            <button className='btn btn-primary mx-3' onClick={handleUpClick} my-2> Upper case </button>
+                    
+                            <button className='btn btn-primary mx-3' onClick={handleloClick}> Lower case </button>
+
+                            <button className='btn btn-primary mx-3' onClick={handleCapitalizeClick}> Capitalized case </button>
+                    
+                            <button className='btn btn-primary mx-3' onClick={handleAlternatingClick}> aLtErNaTiNg case </button>
+
+                            <button className='btn btn-primary mx-3' onClick={handleInversrClick}> Inverse case </button>
+                    
+                            <button className='btn btn-primary mx-3' onClick={handleclearClick}> Clear </button>
+                            
+                            <button className='btn btn-primary mx-3' onClick={handlecopyClick}> Copy </button>
                     </div>
+                    <p></p><p></p>
+        <div className='container My-5'>
+            <h1> Summary of the text </h1>
+            <p> Number of character {text.length} and you can read this in- {0.0075 * text.length} minutes </p> 
+            <p> Number of words {text.split(" ").length}</p>
+            <p> Number of sentance 23     </p>
+            <p> Number of lines 6         </p>
+
+            <h3> Preview</h3>
+            <p> {text.slice(" ")} </p>
         </div>
+    </>
     )
 }
