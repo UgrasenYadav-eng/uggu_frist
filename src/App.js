7701204e0b827dfc9fc0c261138f043520b11b35
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-08-09 06:19:35 +0530

    confussing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import About from './Component/About';
-
 import Navbar from './Component/Navbar.mjs';
 import Textarea from './Component/Textarea';
+import React, { useState } from 'react';
 
 
 function App() {
+  const [mode, setMode]= useState('light'); //wheather dark mode is enable or not.
+  const toggleMode = () => {
+    setMode(Mode => (Mode === 'light' ? 'dark' : 'light'));
+  };
   return (
   <>
-        <Navbar/>
-         <div className='container'>
-         <Textarea heading="Case Converter" />
-        </div> 
-       <About/>
+        <Navbar title="UGGU First" mode={mode} toggleMode={toggleMode} />
+      <div className={`container bg-${mode}`}>
+        <Textarea heading="Case Converter" />
+        <About />
+      </div>
   </>
   
   );
--- src/Component/Navbar.mjs
+++ src/Component/Navbar.mjs
@@ -1,47 +1,49 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Navbar() {
-    return (
-        <div>
-         <div>
-  <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/#">UGGU</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="./Component/Abouts">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          IT Company contect
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/#">Services</a></li>
-            <li><a className="dropdown-item" href="/#">Our Sample Projects</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/#">Contect Us</a></li>
+export default function Navbar(props) {
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/#">UGGU</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">About</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                IT Company Contact
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/#">Services</a></li>
+                <li><a className="dropdown-item" href="/#">Our Sample Projects</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="/#">Contact Us</a></li>
+              </ul>
+            </li>
           </ul>
-        </li>
-      
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-  </div>
+          <div className="d-flex">
+            <div className="form-check form-switch">
+              <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={props.toggleMode} checked={props.mode === 'dark'} />
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                Enable Dark Mode
+              </label>
+            </div>
+          </div>
         </div>
-        
-    )
-    
+      </div>
+    </nav>
+  );
 }
 
+Navbar.propTypes = {
+  mode: PropTypes.string.isRequired,
+  toggleMode: PropTypes.func.isRequired,
+};
